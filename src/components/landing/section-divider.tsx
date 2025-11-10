b73f03f234a109c38a2ba7a9b321c3218582d826
Giel Nijkamp <info@tovy.eu>
2025-11-10 14:44:49 +0000

    make the divider fade out on the left and right side of the line

--- src/components/landing/section-divider.tsx
+++ src/components/landing/section-divider.tsx
@@ -1,7 +1,7 @@
 export function SectionDivider() {
   return (
     <div className="flex justify-center my-16 sm:my-24">
-      <div className="w-2/3 h-px bg-primary"></div>
+      <div className="w-2/3 h-px bg-gradient-to-r from-transparent via-primary to-transparent"></div>
     </div>
   );
 }
