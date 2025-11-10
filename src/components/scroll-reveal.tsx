7969a41e822d1d3a4acb846efdad09ab38bb9e3e
Giel Nijkamp <info@tovy.eu>
2025-11-10 14:19:14 +0000

    make the entry effect of next sections on scroll more powerful/intense

--- src/components/scroll-reveal.tsx
+++ src/components/scroll-reveal.tsx
@@ -50,7 +50,7 @@ export const ScrollReveal = ({
       className={cn(
         "transition-all ease-in-out",
         delay,
-        isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-8",
+        isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-20",
         className
       )}
     >
