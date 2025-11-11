6675d0be2bd97815845a0c477632787e86d12299
Giel Nijkamp <info@tovy.eu>
2025-11-11 11:19:24 +0000

    make the footer the same color as the header.

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="w-full border-t border-border/40">
+    <footer className="w-full border-t border-border/40 bg-background">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 py-8 md:flex-row max-w-6xl px-4 md:px-8">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
