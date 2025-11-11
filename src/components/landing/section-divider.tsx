b30861a381318ec8a90c924e4d2f754ae66087d2
Giel Nijkamp <info@tovy.eu>
2025-11-11 09:20:01 +0000

    Make the divider adapt throughout the widwth of the full page

--- src/components/landing/section-divider.tsx
+++ src/components/landing/section-divider.tsx
@@ -1,7 +1,7 @@
 export function SectionDivider() {
   return (
-    <div className="flex justify-center my-12 sm:my-16">
-      <div className="w-2/3 h-px bg-gradient-to-r from-transparent via-primary to-transparent" style={{
+    <div className="my-12 sm:my-16 w-full">
+      <div className="h-px bg-gradient-to-r from-transparent via-primary to-transparent" style={{
         background: 'linear-gradient(to right, transparent, hsl(var(--primary)), #8F668C, transparent)'
       }}></div>
     </div>
