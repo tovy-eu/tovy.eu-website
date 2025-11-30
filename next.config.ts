1ba99fed24df1310619fa7341c89570b4b2116a6
Giel Nijkamp <info@tovy.eu>
2025-11-30 09:18:28 +0000

    2025-11-30T09:16:00Z [web] <Firebase Studio> Starting preview... 2025-11

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    allowedDevOrigins: ["*.cloudworkstations.dev"],
+  },
   output: 'export',
   typescript: {
     ignoreBuildErrors: true,
