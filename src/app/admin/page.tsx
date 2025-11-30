214bfb47441215502b65a56c3b17fc609970e1cc
Giel Nijkamp <info@tovy.eu>
2025-11-30 08:43:00 +0000

    studio-1546902716:~/studio{main}$ npm run build
    > nextn@0.1.0 build
    > N

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,43 @@
 "use client";
 
 import { useEffect } from "react";
-import config from "../../../public/admin/config.yml";
 
 export default function AdminPage() {
   useEffect(() => {
     (async () => {
       const CMS = (await import("decap-cms-app")).default;
-      CMS.init({ config });
+      const response = await fetch('/admin/config.yml');
+      const config = await response.text();
+      
+      CMS.init({
+        config: {
+          ...CMS.resolveConfig(config, {
+            backend: {
+              name: "git-gateway",
+            },
+            media_folder: "public/uploads",
+            public_folder: "/uploads",
+          })!,
+          collections: [
+            {
+              name: "blog",
+              label: "Blog",
+              folder: "content/blog",
+              create: true,
+              slug: "{{year}}-{{month}}-{{day}}-{{slug}}",
+              fields: [
+                { label: "Title", name: "title", widget: "string" },
+                { label: "Publish Date", name: "date", widget: "datetime" },
+                { label: "Author", name: "author", widget: "string" },
+                { label: "Summary", name: "summary", widget: "text" },
+                { label: "Image ID", name: "image_id", widget: "string" },
+                { label: "Tags", name: "tags", widget: "list", field: { label: "Tag", name: "tag", widget: "string" } },
+                { label: "Body", name: "body", widget: "markdown" },
+              ],
+            },
+          ],
+        },
+      });
     })();
   }, []);
 
