b92dac226784db06da5bccde859f5e04ed198933
Giel Nijkamp <info@tovy.eu>
2025-11-30 08:51:28 +0000

    Isolate with Dynamic Import (Recommended) Move the CMS initialization in

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,45 +1,10 @@
-"use client";
+import dynamic from "next/dynamic";
 
-import { useEffect } from "react";
+const Cms = dynamic(() => import("./Cms"), {
+  ssr: false,
+  loading: () => <p className="h-screen w-full flex items-center justify-center">Loading Admin Panel...</p>,
+});
 
 export default function AdminPage() {
-  useEffect(() => {
-    (async () => {
-      const CMS = (await import("decap-cms-app")).default;
-      const response = await fetch('/admin/config.yml');
-      const config = await response.text();
-      
-      CMS.init({
-        config: {
-          ...CMS.resolveConfig(config, {
-            backend: {
-              name: "git-gateway",
-            },
-            media_folder: "public/uploads",
-            public_folder: "/uploads",
-          })!,
-          collections: [
-            {
-              name: "blog",
-              label: "Blog",
-              folder: "content/blog",
-              create: true,
-              slug: "{{year}}-{{month}}-{{day}}-{{slug}}",
-              fields: [
-                { label: "Title", name: "title", widget: "string" },
-                { label: "Publish Date", name: "date", widget: "datetime" },
-                { label: "Author", name: "author", widget: "string" },
-                { label: "Summary", name: "summary", widget: "text" },
-                { label: "Image ID", name: "image_id", widget: "string" },
-                { label: "Tags", name: "tags", widget: "list", field: { label: "Tag", name: "tag", widget: "string" } },
-                { label: "Body", name: "body", widget: "markdown" },
-              ],
-            },
-          ],
-        },
-      });
-    })();
-  }, []);
-
-  return <div />;
+  return <Cms />;
 }
