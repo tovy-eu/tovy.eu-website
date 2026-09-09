602b8ea1423ad170a648afa7ea656aec84c1f5b4
Giel Nijkamp <gielnijkamp@gmail.com>
2026-09-09 04:10:46 +0200

    and more

---
+++ src/components/DotField.d.ts
@@ -0,0 +1,19 @@
+import { ComponentType } from 'react';
+
+interface DotFieldProps {
+  dotRadius?: number;
+  dotSpacing?: number;
+  cursorRadius?: number;
+  cursorForce?: number;
+  bulgeOnly?: boolean;
+  bulgeStrength?: number;
+  glowRadius?: number;
+  sparkle?: boolean;
+  waveAmplitude?: number;
+  gradientFrom?: string;
+  gradientTo?: string;
+  glowColor?: string;
+}
+
+declare const DotField: ComponentType<DotFieldProps>;
+export default DotField;
